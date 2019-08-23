425fd9046b5d488c34d194089354355b8fdfb166
Guilherme Bastos <cavalcante.guilherme91@gmail.com>
2019-08-22 22:26:21 -0300

    Add api project. Update .gitignore

---
+++ api/estabelecimentos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Estabelecimento(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200, null=False)
+    endereco = models.CharField(max_length=200, null=False)
+    complemento = models.CharField(max_length=100, null=True)
+    cnpj = models.CharField(max_lenght=20, null=False)
+    telefone = models.CharField(max_length=20, null=True)
+    
